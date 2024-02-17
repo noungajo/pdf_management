83b2106591b644be917ba62ba281c972189693d3
noutcha <jonathannoutcha@gmail.com>
2024-02-17 02:49:51 +0100

    refactory projet

--- manage_pdf.py
+++ manage_pdf.py
@@ -1,7 +1,7 @@
 import PyPDF2
 import os
 
-def merge_pdfs(input_folder, output_pdf_path):
+def merge_pdf(input_folder, output_pdf_path):
     pdf_writer = PyPDF2.PdfWriter()
 
     # Iterate through each PDF file in the input folder
@@ -19,8 +19,21 @@ def merge_pdfs(input_folder, output_pdf_path):
     with open(output_pdf_path, 'wb') as output_file:
         pdf_writer.write(output_file)
 
+def split_pdf(input_pdf_path, output_folder):
+    with open(input_pdf_path, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        
+        for page_num in range(len(pdf_reader.pages)):
+            # Create a new PDF writer object for each page
+            pdf_writer = PyPDF2.PdfWriter()
+            page = pdf_reader.pages[page_num]
+            pdf_writer.add_page(page)
+            
+            # Write the new PDF file for each page
+            output_pdf_path = f"{output_folder}/page_{page_num + 1}.pdf"
+            with open(output_pdf_path, 'wb') as output_file:
+                pdf_writer.write(output_file)
+
 # Example usage:
 input_folder = '/home/benjojo/Documents/projets/pdf_management'
 output_pdf_path = 'merged_output.pdf'  # Path to the merged output PDF file
-
-merge_pdfs(input_folder, output_pdf_path)
